e667b82ee57e7068274924118be627570f2dc434
Professor <superelitealfa@gmail.com>
2025-09-12 16:58:19 +0200

    Lite mer test, intro till inventory typ

--- learnTheDogApportJavaGame/src/Items.java
+++ learnTheDogApportJavaGame/src/Items.java
@@ -1,40 +1,69 @@
+import java.util.List;
+
 public class Items {
 
-    public class feedClue {
+    public String item_name;
+    public String description;
+    public boolean canPickUp;
 
-        String description = "Hunden är hungrig, testa att mata hunden med något gott!";
+    public Items (String item_name, String description, boolean canPickUp){
+
+          this.item_name = item_name;
+          this.description = description;
+          this.canPickUp = canPickUp;
+
+    }
+    public String getItem_name() {
+
+        return item_name;
 
     }
 
-    public class whatToFeedClue {
+    public String getDescription() {
 
-        String description = "Hunden älskar köttbullar, Tant Agda brukar ha de. Kanske borde du fråga henne om hon har några extra";
-
-    }
-
-    public class apportClue {
-
-        String description = "Desto mer god mat hunden äter, desto mer ökar chansen att hunden lär sig apport";
+        return description;
 
     }
 
-    public class stick {
+    public boolean canPickUp() {
 
-        String description = "En helt vanlig pinne";
-
-    }
-
-    public static void pickUpItem() {
-
-
-        String item;
-
-
-
-
+        return canPickUp;
 
     }
 
+    public class GameItems {
+
+        public static final Items FEED_CLUE = new Items("ledtråd_mata",
+                "Hunden är hungrig, testa att mata hunden med något gott!", false);
+
+
+        public static final Items WHAT_TO_FEED_CLUE = new Items("ledtråd_köttbullar",
+                "Hunden älskar köttbullar, Tant Agda brukar ha de. Kanske borde du fråga henne om hon har några extra", false);
+
+
+        public static final Items APPORT_CLUE = new Items("ledtråd-apport",
+                "Desto mer god mat hunden äter, desto mer ökar chansen att hunden lär sig apport", false);
+
+        public static final Items STICK = new Items("Pinne",
+                "En helt vanlig pinne", true);
+
+
+    }
+
+        public static void pickUpItem(Items item, List<Items>inventory){
+
+                if(item.canPickUp()){
+
+                    inventory.add(item);
+                    System.out.println("Plockade upp: "+ item.getItem_name());
+
+                } else {
+
+                    System.out.println("Kan inte plocka upp detta föremål");
+
+                }
+
+        }
 
 
 }
