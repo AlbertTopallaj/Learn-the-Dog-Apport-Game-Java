2fa914c6e878e86d95729deccaec57e06354048c
Professor <superelitealfa@gmail.com>
2025-09-12 02:48:05 +0200

    Lite ledtrådar till spelaren

--- learnTheDogApportJavaGame/src/Items.java
+++ learnTheDogApportJavaGame/src/Items.java
@@ -1,2 +1,21 @@
 public class Items {
-}
+
+    public class feedClue {
+
+        String description = "Hunden är hungrig, testa att mata hunden med något gott!";
+
+    }
+
+    public class whatToFeedClue {
+
+        String description = "Hunden älskar köttbullar, Tant Agda brukar ha de. Kanske borde du fråga henne om hon har några extra";
+
+    }
+
+    public class apportClue {
+
+        String description = "Desto mer god mat hunden äter, desto mer ökar chansen att hunden lär sig apport";
+
+    }
+
+
