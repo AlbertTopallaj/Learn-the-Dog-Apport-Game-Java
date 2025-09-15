526006f75c17035744bcb01672b9f9e8053f067b
Sali <salisammo@live.se>
2025-09-15 21:51:24 +0200

    .

---
+++ learnTheDogApportJavaGame/src/Treat.java
@@ -0,0 +1,12 @@
+public class Treat {
+        public String name;
+        public double effect;
+        public String description;
+
+        public Treat(String name, double effect, String description) {
+            this.name = name;
+            this.effect = effect;
+            this.description = description;
+        }
+    }
+}
