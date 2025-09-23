1f4be71f38b5d3b7ee039c9f4f7f3ff1b401c819
Sali <salisammo@live.se>
2025-09-23 21:56:29 +0200

    boolean

--- learnTheDogApportJavaGame/src/Treat.java
+++ learnTheDogApportJavaGame/src/Treat.java
@@ -2,13 +2,13 @@ public class Treat {
     public String name;
     public int effect;
     public String description;
-    // public boolean isHidden; <---- blir massa fel när jag försöker med detta
+    public boolean isHidden;
 
-    public Treat(String name, int effect, String description) {
+    public Treat(String name, int effect, String description, boolean isHidden) {
         this.name = name;
         this.effect = effect;
         this.description = description;
-        // this.isHidden = isHidden;  <---- blir massa fel när jag försöker med detta
+        this.isHidden = isHidden;
     }
 
     public String getName() {
@@ -24,51 +24,60 @@ public class Treat {
     }
 
     public static Treat[] treatArray() {
-        return new Treat[]{
+        return new Treat[] {
                 new Treat(
                         "Morot",
                         1,
-                        "En knaprig men ointressant belöning."
+                        "En knaprig men ointressant belöning.",
+                        false
                 ),
                 new Treat(
                         "Biscuit",
                         3,
-                        "Klassisk hundkex. Smakar okej, ger få poäng"
+                        "Klassisk hundkex. Smakar okej, ger få poäng",
+                        false
                 ),
                 new Treat(
                         "Hundfoder",
                         3,
-                        "Same old, same old. Äts mest av rutin.. Ger få poäng."
+                        "Same old, same old. Äts mest av rutin.. Ger få poäng.",
+                        false
                 ),
                 new Treat(
                         "Ben",
                         5,
-                        "Perfekt att tugga länge på. Ger mer poäng än vanliga treats."
+                        "Perfekt att tugga länge på. Ger mer poäng än vanliga treats.",
+                        false
                 ),
                 new Treat(
                         "Köttbulle",
                         7,
-                        "En saftig liten skatt – hög risk för dregel. Ger bra poäng!"
+                        "En saftig liten skatt – hög risk för dregel. Ger bra poäng!",
+                        false
                 ),
                 new Treat(
                         "Ostbit",
                         7,
-                        "Lyx på hög nivå – hundens version av finmiddag. Ger bra poäng!"
+                        "Lyx på hög nivå – hundens version av finmiddag. Ger bra poäng!",
+                        false
                 ),
                 new Treat(
                         "Lever",
                         10,
-                        "Knaprigt, segt och helt enkelt himmelriket. Ger mycket poäng!"
+                        "Knaprigt, segt och helt enkelt himmelriket. Ger mycket poäng!",
+                        false
                 ),
                 new Treat(
                         "Grisöra",
                         10,
-                        "En sällsynt godsak. Ger mycket poäng!"
+                        "En sällsynt godsak. Ger mycket poäng!",
+                        false
                 ),
                 new Treat(
                         "Special godis",
                         0,
-                        "Hemlig belöning som bara dyker upp i speciella lägen. Dubblar  dina poäng och ditt ego!"
+                        "Hemlig belöning som bara dyker upp i speciella lägen. Dubblar  dina poäng och ditt ego!",
+                        true
                 ),
         }; // Hur dubblar jag poängen som int?
     }
