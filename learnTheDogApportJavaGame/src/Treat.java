ab8d2d91e757d235e3bcdb62d538f284ad3e0ead
Sali <salisammo@live.se>
2025-09-23 21:40:39 +0200

    array

--- learnTheDogApportJavaGame/src/Treat.java
+++ learnTheDogApportJavaGame/src/Treat.java
@@ -2,11 +2,13 @@ public class Treat {
         public String name;
         public int effect;
         public String description;
+        // public boolean isHidden;
 
         public Treat(String name, int effect, String description) {
             this.name = name;
             this.effect = effect;
             this.description = description;
+            // this.isHidden = isHidden;
         }
 
     public String getName() {
@@ -27,44 +29,62 @@ public class Treat {
                 1,
                 "En knaprig men ointressant belöning."
         );
-        return new Treat[0];
+        return new Treat[0]; // blir detta rätt sätt att returnera på?
+
+        new Treat(
+                "Biscuit",
+                3,
+                "Klassisk hundkex. Smakar okej, ger få poäng"
+        );
+        return new Treat[1];
+
+        new Treat(
+                "Hundfoder",
+                3,
+                "Vanlig vardagsmat. Ger få poäng."
+        );
+        return new Treat[2];
+
+        new Treat(
+                "Ben",
+                5,
+                "Perfekt att tugga länge på. Ger mer poäng än vanliga treats."
+        );
+        return new Treat[3];
+
+        new Treat(
+                "Köttbulle",
+                7,
+                "En favorit! Ger bra poäng."
+        );
+        return new Treat[4];
+
+        new Treat(
+                "Ostbit",
+                7,
+                "Supergott! Ger bra poäng."
+        );
+        return new Treat[5];
+
+        new Treat(
+                "Lever",
+                10,
+                "MUMS! Ger mycket poäng!"
+        );
+        return new Treat[6];
+
+        new Treat(
+                "Grisöra",
+                10,
+                "En sällsynt godsak. Ger mycket poäng!"
+        );
+        return new Treat[7];
+
+        new Treat(
+                "Special godis",
+                "Dubblar dina poäng!", // hur skriver jag detta som int?
+                "Hemlig belöning som bara dyker upp i speciella lägen."
+        );
+        return new Treat[8];
     }
 }
-    // ArrayList<String> treat = new ArrayList<>(10); <---- ska vara någon annanstans
-
-/*
-Biscuit
-Effekt: 3 poäng.
-Beskrivning: Klassisk hundkex. Smakar okej, ger få poäng
-
-Hundfoder
-Effekt: 3 poäng.
-Beskrivning: Vanlig vardagsmat. Ger få poäng.
-
-Ben
-Effekt: 5 poäng.
-Beskrivning: Perfekt att tugga länge på. Ger mer poäng än vanliga treats.
-
-Köttbulle
-Effekt: 7 poäng.
-Beskrivning: En favorit! Ger bra poäng.
-
-Ostbit
-Effekt: 7 poäng.
-Beskrivning: Supergott! Ger bra poäng.
-
-Torkad lever
-Effekt: 10 poäng.
-Beskrivning: Lyxig och god belöning. Ger mycket poäng.
-
-Grisöra
-Effekt: 10 poäng.
-Beskrivning: En sällsynt godsak. Ger mycket poäng
-
-
-Specialgodis (gömd)
-Effekt: Dubblar dina poäng.
-Beskrivning: Hemlig belöning som bara dyker upp i speciella lägen.
-(boolean?)
-(isHidden?)
- */
